07f3cee162730af7f8f53917c132a4fe455343a3
phoenix.lv <phoenix.lv@dorry.space>
2018-10-30 16:31:18 +0800

    merge tasks into one cmd

--- jobmgr/job.go
+++ jobmgr/job.go
@@ -1,15 +1,11 @@
 package jobmgr
 
 import (
-    "fmt"
+//    "fmt"
     "log"
     "golang.org/x/crypto/ssh"
-//    "bytes"
     "strings"
     "github.com/gorilla/websocket"
-//    "io"
-//    "encoding/json"
-//    "os"
 )
 
 
@@ -24,7 +20,7 @@ type Job struct {
     Tasks []I_Task `json:tasks`
 }
 
-func (job *Job) GetTaskCMDs() []string {
+func (job *Job) GetTaskCMD() string {
     ts := []string{}
     for index, _ := range job.Tasks {
         task := job.Tasks[index].Serialize()
@@ -34,7 +30,9 @@ func (job *Job) GetTaskCMDs() []string {
         }
         ts = append(ts, task)
     }
-    return ts
+    re :=strings.Join(ts, " && cd && ")
+    log.Println("CMD: ", re)
+    return re
 }
 
 /*
@@ -53,48 +51,48 @@ func (job *Job) Execute(conn *websocket.Conn) {
     job.RPC(dest, authConf, conn)
 }
 
-func syncIO (session *ssh.Session, conn *websocket.Conn) {
-    go func(*ssh.Session, *websocket.Conn) {
+func syncIO (session *ssh.Session, conn *websocket.Conn, sshConn *ssh.Client) {
+    go func(*ssh.Session, *websocket.Conn, *ssh.Client) {
         sessionReader, err := session.StdoutPipe()
         if err != nil {
           log.Fatal(err)
         }
 
-        fmt.Println("======================== Sync session output ======================")
+        log.Println("======================== Sync session output ======================")
         defer func() {
-            fmt.Println("======================== output: end ======================")
+            log.Println("======================== output: end ======================")
+            conn.Close()
+            sshConn.Close()
             session.Close()
         }()
 
         for {
-            fmt.Println("say anything about output")
             // set io.Writer of websocket
             outbuf := make([]byte, 4096)
             outn, err := sessionReader.Read(outbuf)
             if err != nil {
-                log.Print(err)
-                fmt.Println("sshReader: ", err)
+                log.Println("sshReader: ", err)
                 return
             }
             err = conn.WriteMessage(websocket.TextMessage, outbuf[:outn])
             if err != nil {
-                log.Print(err)
-                fmt.Println("connWriter: ", err)
-                fmt.Println(session)
+                log.Println("connWriter: ", err)
                 return
             }
         }
-    }(session, conn)
+    }(session, conn, sshConn)
 
-    go func(*ssh.Session, *websocket.Conn) {
+    go func(*ssh.Session, *websocket.Conn, *ssh.Client) {
         sessionWriter, err := session.StdinPipe()
         if err != nil {
             log.Fatal(err)
         }
 
-        fmt.Println("======================== Sync session input ======================")
+        log.Println("======================== Sync session input ======================")
         defer func() {
-            fmt.Println("======================== input: end ======================")
+            log.Println("======================== input: end ======================")
+            conn.Close()
+            sshConn.Close()
             session.Close()
         }()
 
@@ -102,27 +100,23 @@ func syncIO (session *ssh.Session, conn *websocket.Conn) {
             // set up io.Reader of websocket
             _, reader, err := conn.NextReader()
             if err != nil {
-                log.Print(err)
-                fmt.Println("connReaderCreator: ", err)
+                log.Println("connReaderCreator: ", err)
                 return
             }
             buf := make([]byte, 1024)
             n, err := reader.Read(buf)
             if err != nil {
-                log.Print(err)
-                fmt.Println("connReader: ", err)
+                log.Print("connReader: ", err)
                 return
             }
             _, err = sessionWriter.Write(buf[:n])
             if err != nil {
-                log.Print(err)
-                fmt.Println("sshWriter: ", err)
-                fmt.Println(session)
+                log.Print("sshWriter: ", err)
                 conn.WriteMessage(websocket.TextMessage, []byte(err.Error()))
                 return
             }
         }
-    }(session, conn)
+    }(session, conn, sshConn)
 }
 
 func (job *Job) RPC(dest string, authConf *ssh.ClientConfig, conn *websocket.Conn) {
@@ -130,53 +124,28 @@ func (job *Job) RPC(dest string, authConf *ssh.ClientConfig, conn *websocket.Con
     if err != nil {
         log.Fatal("Failed to dial: ", err)
     }
-    /*
-    defer func() {
-      fmt.Println("============================= Close sshConn ======================")
-      sshConn.Close()
-    }()
-    */
-    // Each ClientConn can support multiple interactive sessions,
-    // represented by a Session.
-    tasks := job.Tasks
-    //sessionCounter := 0
-    sessionNum := len(tasks)
-    ss := make([]*ssh.Session, sessionNum)
-    /*
-    go func() {
-      for {
-        if sessionCounter == sessionNum {
-          sshConn.Close()
-          conn.Close()
-        }
-      }
-    }()
-    */
-    for index, _ := range tasks {
-        // Once a Session is created, you can execute a single command on
-        // the remote side using the Run method.
-        // Set New session
-        ss[index], err = sshConn.NewSession()
-        if err != nil {
-            log.Fatal("Failed to create session: ", err)
-        }
-        modes := ssh.TerminalModes {
-          ssh.ECHO:          0,     // disable echoing
-          ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-          ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-        }
-        // Request pseudo terminal
-        if err := ss[index].RequestPty("xterm", 40, 80, modes); err != nil {
-            log.Fatal("request for pseudo terminal failed: ", err)
-        }
+    // Once a Session is created, you can execute a single command on
+    // the remote side using the Run method.
+    // Set New session
+    ss, err := sshConn.NewSession()
+    if err != nil {
+        log.Fatal("Failed to create session: ", err)
+    }
+    modes := ssh.TerminalModes {
+      ssh.ECHO:          0,     // disable echoing
+      ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+      ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+    }
+    // Request pseudo terminal
+    if err := ss.RequestPty("xterm", 40, 80, modes); err != nil {
+        log.Fatal("request for pseudo terminal failed: ", err)
+    }
 
-        syncIO(ss[index], conn)
-        fmt.Println(ss[index])
-        cmd := job.GetTaskCMDs()[index]
-        if err := ss[index].Run(cmd); err != nil {
-            log.Fatal("failed to run cmd: ", err)
-        }
-     }
+    syncIO(ss, conn, sshConn)
+    cmd := job.GetTaskCMD()
+    if err := ss.Run(cmd); err != nil {
+        log.Fatal("failed to run cmd: ", err)
+    }
 }
 
 
